0baa8afccab66be5feed1f7f270bf286776998b3
khabibullo <khabibullo@gmail.com>
2021-09-27 06:29:01 +0500

    JS-9.1

--- script.js
+++ script.js
@@ -20,7 +20,25 @@ for (let i = 0; i < squareNumber; i++) {
 }
 
 function setColor(element) {
-    const color = randomCol;
+    const color = '';
+    function randomCol (){
+        let num = Math.floor(Math.random() * (5 + 1 - 1) + 1)
+        return num;
+    }
+
+    let exampleCol = randomCol();
+
+    if(exampleCol == 1) {
+        color = 'red'
+    }else if(exampleCol == 2) {
+        color = 'lime'
+    }else if (exampleCol == 3) {
+        color = 'fuchsia'
+    }else if (exampleCol == 4) {
+        color = 'yellow'
+    }else if (exampleCol == 5) {
+        color = 'aqua'
+    }
     element.style.background = color;
     element.style.boxShadow = `0 0 2px ${color}`;
 }
@@ -31,22 +49,5 @@ function removeColor(element) {
     element.style.boxShadow = '0 0 2px #000';
 }
 
-function randomCol() {
-    return Math.floor(Math.random() * (6 - 1) - 1)
-}
 
-for(let i = 0; i < 500; i++){
-    let exampleCol = randomCol()
-}
 
-if(exampleCol == 1) {
-    exampleCol = '#00ffff'
-}else if(exampleCol == 2) {
-    exampleCol = '#ff0000'
-}else if (exampleCol == 3) {
-    exampleCol = '#ffff00'
-}else if (exampleCol == 4) {
-    exampleCol = '#008000'
-}else if (exampleCol == 5) {
-    exampleCol = '#800080'
-}
